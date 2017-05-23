7a41bde37b10a1df54662e51817f948a61adafb6
linyuyoung <linyuyoung@gmail.com>
2017-05-23 16:56:41 +0800

    Update liquidFillGauge.js

--- liquidFillGauge.js
+++ liquidFillGauge.js
@@ -18,10 +18,10 @@ function liquidFillGaugeDefaultSettings(){
         circleThickness: 0.05, // The outer circle thickness as a percentage of it's radius.
         circleFillGap: 0.05, // The size of the gap between the outer circle and wave circle as a percentage of the outer circles radius.
         circleColor: "#178BCA", // The color of the outer circle.
-        waveHeight: 0.05, // The wave height as a percentage of the radius of the wave circle.
+        waveHeight: 0.1, // The wave height as a percentage of the radius of the wave circle.
         waveCount: 1, // The number of full waves per width of the wave circle.
-        waveRiseTime: 1000, // The amount of time in milliseconds for the wave to rise from 0 to it's final height.
-        waveAnimateTime: 18000, // The amount of time in milliseconds for a full wave to enter the wave circle.
+        waveRiseTime: 2500, // The amount of time in milliseconds for the wave to rise from 0 to it's final height.
+        waveAnimateTime: 1000, // The amount of time in milliseconds for a full wave to enter the wave circle.
         waveRise: true, // Control if the wave should rise from 0 to it's full height, or start at it's full height.
         waveHeightScaling: true, // Controls wave size scaling at low and high fill percentages. When true, wave height reaches it's maximum at 50% fill, and minimum at 0% and 100% fill. This helps to prevent the wave from making the wave circle from appear totally full or empty when near it's minimum or maximum fill.
         waveAnimate: true, // Controls if the wave scrolls or is static.
@@ -32,9 +32,10 @@ function liquidFillGaugeDefaultSettings(){
         valueCountUp: true, // If true, the displayed value counts up from 0 to it's final value upon loading. If false, the final value is displayed.
         displayPercent: true, // If true, a % symbol is displayed after the value.
         textColor: "#290254", // The color of the value text when the wave does not overlap it.
-        waveTextColor: "#A4DBf8" // The color of the value text when the wave overlaps it.
+        waveTextColor: "#000000" // The color of the value text when the wave overlaps it.
     };
 }
+
 
 function loadLiquidFillGauge(elementId, value, config) {
     if(config == null) config = liquidFillGaugeDefaultSettings();
